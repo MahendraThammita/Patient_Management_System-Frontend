cbb5623fe61da754e5f52bee42a81a5450f4a651
UdithaJ <udithaj.98@gmail.com>
2021-08-02 20:34:31 +0530

    Route added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import RegisterStaff from './components/Support_Staff/Staff_Registration/Registe
 import ReceptionistRegister from "./components/Receptionist/Register";
 import ReceptionistLogin from "./components/Receptionist/Login";
 import AddDoctor from "./components/Receptionist/AddDoctor";
+import Dashboard from "./components/Receptionist/Dashboard/Base";
 
 function App() {
   return (
@@ -33,6 +34,9 @@ function App() {
           </Route>
           <Route path="/receptionist-login">
               <ReceptionistLogin />
+          </Route>
+          <Route path="/receptionist-dashboard">
+              <Dashboard />
           </Route>
       </Switch>
     </div>
---
+++ src/components/Receptionist/Dashboard/Base.jsx
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from "react";
+import Doctors from "./Doctors";
+
+
+const Dashboard = () => {
+
+    return(
+        <div>
+            <Doctors/>
+        </div>
+    )
+
+}
+
+export default Dashboard;
