cbab1579d2975fddea06297b913b71eac8c8fab4
UdithaJ <udithaj.98@gmail.com>
2021-08-17 12:10:37 +0530

    Appointment Card views

--- src/components/Receptionist/Dashboard/Base.jsx
+++ src/components/Receptionist/Dashboard/Base.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import Doctors from "./Doctors";
 import CurrentAppointment from "./CurrentAppointment";
 import '../../../assets/css/uditha.css'
+import PendingAppointment from "./PendingAppointment";
 
 
 const Dashboard = () => {
@@ -14,6 +15,7 @@ const Dashboard = () => {
         <div className="uditha-dashboard-align">
             <Doctors/>
             <CurrentAppointment/>
+            <PendingAppointment/>
         </div>
     )
 
--- src/components/Receptionist/Dashboard/Doctors.jsx
+++ src/components/Receptionist/Dashboard/Doctors.jsx
@@ -11,7 +11,7 @@ const Doctors = () => {
     const onSearch = value => console.log(value);
 
     return(
-        <div style={{float:"left", marginLeft: '10px', marginTop:'5%'}}>
+        <div style={{float:"left", marginLeft: '60px', marginTop:'5%'}}>
             <Search style={{marginBottom: '5px'}} placeholder="Search Doctors" onSearch={onSearch} enterButton />
             <Card
 
@@ -19,7 +19,7 @@ const Doctors = () => {
                 cover={
                     <img
                         alt="example"
-                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                        src="https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
                     />
                 }
             >
