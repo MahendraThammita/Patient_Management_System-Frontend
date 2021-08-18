1cdcd7e677d8b4e3e153e05c7d1ad7e26c32bc07
unknown <dhmmpthammita@gmail.com>
2021-08-18 10:22:28 +0530

    Nursing Dashboard UI 100% #109

--- src/components/Support_Staff/Nurse_Dashboard/NurseDashboard.jsx
+++ src/components/Support_Staff/Nurse_Dashboard/NurseDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Form, Row, Col, Button, Layout, Typography, Divider, Menu, Badge, Avatar } from 'antd';
-import { UserOutlined, NotificationOutlined, LaptopOutlined, EditTwoTone } from '@ant-design/icons';
+import { Form, Row, Col, Image, Layout, Typography, Dropdown, Menu, Badge, Avatar, message } from 'antd';
+import { TabletFilled, FileAddFilled, HomeFilled, BellOutlined, DownOutlined, LogoutOutlined, DashboardOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import '../../../assets/css/mahen_general.css';
 import WelcomeSection from '../DashboardCommon/WelcomeSection'
@@ -9,6 +9,7 @@ import AppointmentChart from './AppointmentChart'
 import CollectionsChart from './CollectionsChart'
 import OverviewCard from '../DashboardCommon/OverviewCard'
 import JobQueueComponant from './JobQueueComponant'
+import Logo from '../../../assets/img/pmslogo.png'
 
 const { Title, Text, Link } = Typography;
 const { SubMenu } = Menu;
@@ -19,32 +20,85 @@ export default class NurseDashboard extends Component {
         this.state = {
             form: Form
         }
+        this.onDropdownMenuClick = this.onDropdownMenuClick.bind(this);
+
     }
 
+    onDropdownMenuClick = ({ key }) => {
+        message.info(`Click on item ${key}`);
+    };
 
     render() {
-
+        const menu = (
+            <Menu onClick={this.onDropdownMenuClick}>
+                <Menu.Item key="1" icon={<LogoutOutlined />}>Log Out</Menu.Item>
+            </Menu>
+        );
         return (
             <div>
                 <Layout>
                     <Header className="header">
-                        <div className="logo" />
+
+                        <Row justify='start' align='middle'>
+                            <Col span={6}>
+                                <Row justify='space-between' align='middle'>
+                                    <Col span={6}>
+                                        <div className="logo" >
+                                            <Image
+                                                width={100}
+                                                src={Logo}
+                                            />
+                                        </div>
+                                    </Col>
+                                    <Col span={16}>
+                                        <Title level={3} style={{ color: 'white', marginTop: 25 , marginLeft:15 }}>PMS Health Care</Title>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col span={3} offset={15}>
+                                <Row>
+                                    <Col span={8}>
+                                        <Badge count={5}>
+                                            <Avatar size="default" icon={<BellOutlined />} />
+                                        </Badge>
+                                    </Col>
+                                    <Col span={16}>
+                                        <Dropdown overlay={menu}>
+                                            <a className="ant-dropdown-link" style={{ color: 'white' }} onClick={e => e.preventDefault()}>
+                                                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                                                Sara Meti <DownOutlined />
+                                            </a>
+                                        </Dropdown>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
 
                     </Header>
                     <Layout>
                         <Sider width={200} className="site-layout-background">
+                            <div className="logo">
+                                <Row justify='center'>
+                                <DashboardOutlined style={{marginTop:5 , marginRight:15}} /><Text strong>Nursing Dashboard</Text>
+                                </Row>
+                                
+                            </div>
                             <Menu
+                                theme="dark"
                                 mode="inline"
                                 defaultSelectedKeys={['1']}
                                 defaultOpenKeys={['sub1']}
                                 style={{ height: '100%', borderRight: 0 }}
                             >
-                                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                                    <Menu.Item key="1">option1</Menu.Item>
-                                    <Menu.Item key="2">option2</Menu.Item>
-                                    <Menu.Item key="3">option3</Menu.Item>
-                                    <Menu.Item key="4">option4</Menu.Item>
-                                </SubMenu>
+                                <Menu.Item key="1" icon={<HomeFilled />}>
+                                    Home
+                                </Menu.Item>
+                                <Menu.Item key="2" icon={<TabletFilled />}>
+                                    Appointments
+                                </Menu.Item>
+                                <Menu.Item key="3" icon={<FileAddFilled />}>
+                                    Sample Collections
+                                </Menu.Item>
                             </Menu>
                         </Sider>
                         <Layout style={{ padding: '0 24px 24px' }}>
@@ -67,7 +121,7 @@ export default class NurseDashboard extends Component {
                                     </Row>
                                     <Row>
                                         <Col span={24}>
-                                            <JobQueueComponant/>
+                                            <JobQueueComponant />
                                         </Col>
                                     </Row>
                                 </Col>
